48b0b42bf580f7fb5c306fe3572d2a67e20198cc
Måns Sandström <mans.sandstrom@gmail.com>
2017-10-01 23:31:12 +0200

    Removed unused parameter

--- lib/threesmodel/solvers/highest_paying_next_move_solver.ex
+++ lib/threesmodel/solvers/highest_paying_next_move_solver.ex
@@ -6,7 +6,7 @@ defmodule HighestPayingNextMoveSolver do
       {direction, _} = Threesmodelex.possible_folds(board)
         |> Enum.map(fn(direction) -> {direction, GameBoardFolder.fold(board, direction)} end)
         |> Enum.map(fn({dir, b}) -> {dir, GameScorer.score_for(b)} end)
-        |> Enum.max_by(fn({dir, score}) -> score end)
+        |> Enum.max_by(fn({_, score}) -> score end)
       Threesmodelex.fold(board, direction)
     end)
   end
