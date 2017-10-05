d512e2dd4b5ed4bccb74bb3934d8eadecf3fdb06
Måns Sandström <mans.sandstrom@gmail.com>
2017-10-05 23:08:01 +0200

    Fixed histogram reports and renamed solver

--- lib/threesmodel/solvers/solver_base.ex
+++ lib/threesmodel/solvers/solver_base.ex
@@ -6,24 +6,40 @@ defmodule SolverBase do
   end
 
   def solve(board, false, fun) do
-    folded_board = fun.(board)
-    solve(folded_board, Threesmodelex.is_game_over?(folded_board), fun)
+    b = fun.(board)
+    solve(b, Threesmodelex.is_game_over?(b), fun)
   end
 
   def solve(board, true, _) do
+    send :stats, {:high_cell, NextNumberDeterminator.highest_cell_on_board(board)}
     [{:score, Threesmodelex.score_for_board(board)},{:board, board}]
   end
 
   def run_many(times, fun) do
-    1..times
+    {:ok, pid} = Task.start_link(fn -> SolverBase.collect(%{3 => 0, 6 => 0, 12 => 0, 24 => 0, 48 => 0, 96 => 0, 192 => 0, 384 => 0, 768 => 0, 1536 => 0, 3072 => 0, 6144 => 0, 12288 => 0, 24576 => 0}) end)
+    Process.register(pid, :stats)
+    best_game = 1..times
       |> pmap(fn(_) -> run(fun) end)
       |> Enum.max_by(fn([{:score, score}, _]) -> score end)
+    IO.puts("*****   BEST GAME   *****")
+    IO.inspect(best_game)
+    IO.puts("*****   HISTOGRAM   *****")
+    send :stats, {:do_end}
   end
 
   def pmap(collection, func) do
     collection
       |> Enum.map(&(Task.async(fn -> func.(&1) end)))
-      |> Enum.map(fn(task) -> Task.await(task, 100000) end)
+      |> Enum.map(&Task.await/1)
   end
 
+  def collect(state) do
+    receive do
+      {:high_cell, value} -> collect(Enum.map(state, fn({key, val}) -> if key == value do {key, val + 1} else {key, val} end end))
+      {:do_end} -> IO.inspect(state)
+      junk ->
+        IO.inspect(junk)
+        collect(state)
+    end
+  end
 end
